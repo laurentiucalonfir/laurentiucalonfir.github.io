75d87ee87bc4774109399f4cae56be9906560dfd
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-03-20 01:19:22 +0200

    Made change base_arch

--- base_arch.sh
+++ base_arch.sh
@@ -11,7 +11,7 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:x | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager linux-headers
+pacman -S --noconfirm grub efibootmgr networkmanager linux-headers pacman-contrib
 mkdir /boot/efi
 mount /dev/sda1 /boot/efi
 
