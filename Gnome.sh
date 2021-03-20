ae986da5d24cbd8b8197c440a4bd77ee6e1d2cb8
laurentiucalonfir <59648097+laurentiucalonfir@users.noreply.github.com>
2021-03-20 16:42:00 +0200

    Update Gnome.sh

--- Gnome.sh
+++ Gnome.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
 sudo reflector -c Romania -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
+
 
 sudo pacman -S --noconfirm xorg gdm baobab eog evince file-roller gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-getting-started-docs gnome-keyring gnome-logs gnome-maps gnome-menus gnome-music gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects 	grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus rygel simple-scan sushi tracker tracker3 tracker3-miners tracker-miners vino xdg-user-dirs-gtk totem gnome-tweaks chrome-gnome-shell 
 
