e85001d4f8195c30adb79e96fdca70da503fdfef
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-05-21 21:59:55 +0300

    cinnamon change

--- cinnamon.sh
+++ cinnamon.sh
@@ -8,7 +8,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 
-sudo pacman -S --noconfirm xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon arc-gtk-theme arc-icon-theme xed metacity gnome-terminal
+sudo pacman -S --noconfirm xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon arc-gtk-theme arc-icon-theme xed metacity gnome-terminal gnome-system-monitor gnome-disk-utility
 
 sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
