1bd6637e2df0df7f3ca26a6d446d95475d3fedde
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-05-22 09:45:45 +0300

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -8,7 +8,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
 
-sudo pacman -S --noconfirm xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon arc-gtk-theme arc-icon-theme xed metacity gnome-terminal gnome-system-monitor gnome-disk-utility
+sudo pacman -S --noconfirm xorg lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon arc-gtk-theme arc-icon-theme xed metacity gnome-terminal gnome-system-monitor gnome-disk-utility imwheel numlockx
+
+yay -S --noconfirm google-chrome
+yay -S --noconfirm stremio
+yay -S --noconfirm wol-systemd
 
 sudo systemctl enable lightdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
