c69316597567fa06405335c7066ea471108bd2d8
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-05-23 15:03:35 +0300

    Update basearch.sh

--- basearch.sh
+++ basearch.sh
@@ -56,7 +56,7 @@ arch-chroot /mnt pacman -S --noconfirm grub efibootmgr networkmanager linux-head
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-useradd -m laurentiu
+arch-chroot /mnt useradd -m laurentiu
 printf "x\nx" | arch-chroot /mnt passwd laurentiu
 
 echo "laurentiu ALL=(ALL) ALL" >> /etc/sudoers.d/laurentiu
