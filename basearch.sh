8feff1a10497cd52f6cc839db69f9d1cd62a343b
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-05-23 10:42:44 +0300

    Update basearch.sh

--- basearch.sh
+++ basearch.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
+(
+echo g # Create a new empty gpt partition table
+echo n # Add a new partition
+echo
+echo   # First sector (Accept default: 1)
+echo +512M  # Last sector (Accept default: varies) 
+echo t
+echo 1
+echo n # Add a new partition
+echo 2 # Partition number
+echo   # First sector
+echo +4G # Last sector
+echo t
+echo 2
+echo 19
+echo n # Add a new partition
+echo   # First sector
+echo
+echo   # Last sector (Accept default: varies)
+echo w # Write changes
+) | fdisk /dev/sda
 
-ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
-hwclock --systohc
+mkfs.vfat /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+pacstrap /mnt base base-devel linux-lts linux-firmware nano
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
+arch-chroot /mnt hwclock --systohc
 sed -i '177s/.//' /etc/locale.gen
-locale-gen
+arch-chroot /mnt locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -12,15 +43,15 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 echo root:x | chpasswd
 
-pacman -S --noconfirm grub efibootmgr networkmanager linux-headers pacman-contrib git
+arch-chroot /mnt pacman -S --noconfirm grub efibootmgr networkmanager linux-headers pacman-contrib git
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable NetworkManager
+arch-chroot /mnt systemctl enable NetworkManager
 
 useradd -m laurentiu
 echo laurentiu:x | chpasswd
