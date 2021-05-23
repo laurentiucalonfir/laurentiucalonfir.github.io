a3c0eaacc01b02da65da84980643b486f8216d59
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-05-23 10:18:15 +0300

    Update basearch.sh

--- basearch.sh
+++ basearch.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
-(
-echo g # Create a new empty DOS partition table
-echo n # Add a new partition
-echo   # First sector (Accept default: 1)
-echo +512M  # Last sector (Accept default: varies) 
-echo t
-echo 1
-echo n # Add a new partition
-echo 2 # Partition number
-echo   # First sector
-echo +4G # Last sector
-echo t
-echo 2
-echo 19
-echo n # Add a new partition
-echo   # First sector
-echo   # Last sector (Accept default: varies)
-echo w # Write changes
-) | sudo fdisk
 
 
 ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
