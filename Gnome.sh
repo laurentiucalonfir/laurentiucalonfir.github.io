2a2bc911dfe08471967e98104e167b2b904b675d
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-06-06 08:46:59 +0300

    Update Gnome.sh

--- Gnome.sh
+++ Gnome.sh
@@ -4,15 +4,15 @@ sudo timedatectl set-ntp true
 sudo hwclock --systohc
 cd /tmp
 
-git clone https://aur.archlinux.org/paru.git
-cd paru/
+git clone https://aur.archlinux.org/yay.git
+cd yay/
 makepkg -si --noconfirm
 cd /
 
 sudo pacman -S --noconfirm xorg gdm baobab eog evince file-roller gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-keyring gnome-logs gnome-maps gnome-menus gnome-music gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects 	grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus rygel simple-scan sushi tracker tracker3 tracker3-miners tracker-miners vino xdg-user-dirs-gtk totem gnome-tweaks neofetch imwheel seahorse ttf-roboto ttf-liberation ttf-dejavu
 
-paru -S noconfirm nvidia-340xx-utils
-paru -S noconfirm nvidia-340xx-lts
+yay -S noconfirm nvidia-340xx-utils
+yay -S noconfirm nvidia-340xx-lts
 
 sudo systemctl enable gdm
 
