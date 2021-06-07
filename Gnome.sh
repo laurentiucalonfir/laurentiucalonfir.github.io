0433b778441602393a15b5f17b504fe84958f432
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-06-07 22:06:39 +0300

    Update Gnome.sh

--- Gnome.sh
+++ Gnome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S --noconfirm reflector
-sudo reflector -c Romania -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector -c Romania -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
