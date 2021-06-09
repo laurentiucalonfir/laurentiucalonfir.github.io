05e7da3000970d1b4d5aaf99c4d0ea51ad1a21c5
laurentiucalonfir <laurentiucalonfir@gmail.com>
2021-06-09 20:59:27 +0300

    Update Gnome.sh

--- Gnome.sh
+++ Gnome.sh
@@ -15,8 +15,6 @@ cd /
 
 sudo pacman -S --noconfirm xorg gdm baobab eog evince file-roller gedit gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-disk-utility gnome-documents gnome-font-viewer gnome-keyring gnome-logs gnome-maps gnome-menus gnome-music gnome-photos gnome-remote-desktop gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra gnome-user-docs gnome-user-share gnome-video-effects 	grilo-plugins gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb mutter nautilus rygel simple-scan sushi tracker tracker3 tracker3-miners tracker-miners vino xdg-user-dirs-gtk totem gnome-tweaks neofetch imwheel seahorse ttf-roboto ttf-liberation ttf-dejavu
 
-yay -S --noconfirm nvidia-340xx-utils
-yay -S --noconfirm nvidia-340xx-lts
 
 sudo systemctl enable gdm
 
