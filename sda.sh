69c1f32bb763205d3c0f987edc5a8147b4205065
laurentiucalonfir <laurentiucalonfir@gmail.com>
2024-05-27 13:35:55 +0300

    Update sda.sh

--- sda.sh
+++ sda.sh
@@ -1,73 +1,85 @@
 #!/bin/bash
 
-(
-echo g # Create a new empty gpt partition table
-echo n # Add a new partition
-echo
-echo   # First sector (Accept default: 1)
-echo +512M  # Last sector (Accept default: varies) 
-echo t
-echo 1
-echo n # Add a new partition
-echo 2 # Partition number
-echo   # First sector
-echo +4G # Last sector
-echo t
-echo 2
-echo 19
-echo n # Add a new partition
-echo   # First sector
-echo
-echo   # Last sector (Accept default: varies)
-echo w # Write changes
-) | fdisk /dev/sda
+# Variables
+DISK="/dev/sda"
+EFI_PART="${DISK}1"
+ROOT_PART="${DISK}2"
+EFI_MOUNT="/mnt/boot"
+ROOT_MOUNT="/mnt"
 
-mkfs.vfat /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
-swapon /dev/sda2
-mount /dev/sda3 /mnt
-mkdir -p /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+# 1. Partition the disk
+echo "Partitioning the disk..."
+parted -s $DISK mklabel gpt
+parted -s $DISK mkpart primary fat32 1MiB 512MiB
+parted -s $DISK set 1 esp on
+parted -s $DISK mkpart primary ext4 512MiB 100%
 
-pacstrap /mnt base base-devel linux-lts linux-firmware nano
-genfstab -U /mnt >> /mnt/etc/fstab
+# 2. Format the partitions
+echo "Formatting the partitions..."
+mkfs.fat -F32 $EFI_PART
+mkfs.ext4 $ROOT_PART
 
+# 3. Mount the filesystems
+echo "Mounting the filesystems..."
+mount $ROOT_PART $ROOT_MOUNT
+mkdir -p $EFI_MOUNT
+mount $EFI_PART $EFI_MOUNT
 
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
-arch-chroot /mnt hwclock --systohc
-sed -i '177s/.//' /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
-echo "LANG=en_US.UTF-8" >> /mnt/etc/locale.conf
-echo "arch" >> /mnt/etc/hostname
-echo "127.0.0.1 localhost" >> /mnt/etc/hosts
-echo "::1       localhost" >> /mnt/etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /mnt/etc/hosts
-arch-chroot /mnt echo root:x | chpasswd
-printf "x\nx" | arch-chroot /mnt passwd
+# 4. Install the base system
+echo "Installing the base system..."
+pacstrap $ROOT_MOUNT base linux linux-firmware
 
+# 5. Generate fstab
+echo "Generating fstab..."
+genfstab -U $ROOT_MOUNT >> $ROOT_MOUNT/etc/fstab
 
-arch-chroot /mnt pacman -S --noconfirm grub efibootmgr networkmanager pacman-contrib git
+# 6. Chroot into the new system
+echo "Chrooting into the new system..."
+arch-chroot $ROOT_MOUNT /bin/bash <<EOF
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+# 7. Set the time zone
+echo "Setting the time zone..."
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
 
+# 8. Localization
+echo "Setting up localization..."
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+# 9. Network configuration
+echo "Setting up the hostname and hosts file..."
+echo "myhostname" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 myhostname.localdomain myhostname" >> /etc/hosts
 
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
-arch-chroot /mnt systemctl enable NetworkManager
+# 10. Set the root password
+echo "Setting the root password..."
+echo "root:password" | chpasswd
 
+# 11. Install and configure systemd-boot
+echo "Installing systemd-boot..."
+bootctl --path=$EFI_MOUNT install
 
-arch-chroot /mnt useradd -m laurentiu
-printf "x\nx" | arch-chroot /mnt  passwd laurentiu
-echo "laurentiu ALL=(ALL) ALL" >> /mnt/etc/sudoers.d/laurentiu
+# Create loader configuration
+echo "default arch" > /boot/loader/loader.conf
+echo "timeout 3" >> /boot/loader/loader.conf
+echo "editor 0" >> /boot/loader/loader.conf
 
+# Create boot entry
+PARTUUID=$(blkid -s PARTUUID -o value $ROOT_PART)
+echo "title   Arch Linux" > /boot/loader/entries/arch.conf
+echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options root=PARTUUID=$PARTUUID rw" >> /boot/loader/entries/arch.conf
 
+EOF
 
-umount -a
-echo -e "\e[1;32mRebooting in 5..4..3..2..1\e[0m"
-sleep 5
+# 12. Unmount filesystems and reboot
+echo "Unmounting filesystems and rebooting..."
+umount -R $ROOT_MOUNT
 reboot
 
-
+echo "Arch Linux installation with systemd-boot is complete!"
