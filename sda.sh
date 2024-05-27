babfb7d6f30431496a5f77bcdb3de2f5f31251e1
laurentiucalonfir <laurentiucalonfir@gmail.com>
2024-05-27 16:40:11 +0300

    Update sda.sh

--- sda.sh
+++ sda.sh
@@ -1,85 +1,95 @@
 #!/bin/bash
 
 # Variables
-DISK="/dev/sda"
+DISK="/dev/sdX"
 EFI_PART="${DISK}1"
 ROOT_PART="${DISK}2"
-EFI_MOUNT="/mnt/boot"
-ROOT_MOUNT="/mnt"
+HOSTNAME="archlinux"
+USERNAME="user"
 
-# 1. Partition the disk
-echo "Partitioning the disk..."
+# Update system clock
+timedatectl set-ntp true
+
+# Partition the disk
 parted -s $DISK mklabel gpt
-parted -s $DISK mkpart primary fat32 1MiB 512MiB
+parted -s $DISK mkpart primary fat32 1MiB 513MiB
 parted -s $DISK set 1 esp on
-parted -s $DISK mkpart primary ext4 512MiB 100%
+parted -s $DISK mkpart primary ext4 513MiB 100%
 
-# 2. Format the partitions
-echo "Formatting the partitions..."
+# Format the partitions
 mkfs.fat -F32 $EFI_PART
 mkfs.ext4 $ROOT_PART
 
-# 3. Mount the filesystems
-echo "Mounting the filesystems..."
-mount $ROOT_PART $ROOT_MOUNT
-mkdir -p $EFI_MOUNT
-mount $EFI_PART $EFI_MOUNT
+# Mount the file systems
+mount $ROOT_PART /mnt
+mkdir /mnt/boot
+mount $EFI_PART /mnt/boot
 
-# 4. Install the base system
-echo "Installing the base system..."
-pacstrap $ROOT_MOUNT base linux linux-firmware
+# Install base system and linux-lts kernel
+pacstrap /mnt base linux-lts linux-lts-headers linux-firmware
 
-# 5. Generate fstab
-echo "Generating fstab..."
-genfstab -U $ROOT_MOUNT >> $ROOT_MOUNT/etc/fstab
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
-# 6. Chroot into the new system
-echo "Chrooting into the new system..."
-arch-chroot $ROOT_MOUNT /bin/bash <<EOF
+# Chroot into the new system
+arch-chroot /mnt /bin/bash <<EOF
 
-# 7. Set the time zone
-echo "Setting the time zone..."
+# Set the time zone
 ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
 hwclock --systohc
 
-# 8. Localization
-echo "Setting up localization..."
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+# Localization
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-# 9. Network configuration
-echo "Setting up the hostname and hosts file..."
-echo "myhostname" > /etc/hostname
-echo "127.0.0.1 localhost" >> /etc/hosts
-echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 myhostname.localdomain myhostname" >> /etc/hosts
+# Network configuration
+echo "$HOSTNAME" > /etc/hostname
+cat <<EOT > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+EOT
 
-# 10. Set the root password
-echo "Setting the root password..."
-echo "root:password" | chpasswd
+# Initramfs
+mkinitcpio -P
 
-# 11. Install and configure systemd-boot
-echo "Installing systemd-boot..."
-bootctl --path=$EFI_MOUNT install
+# Root password
+echo "Set root password:"
+passwd
 
-# Create loader configuration
-echo "default arch" > /boot/loader/loader.conf
-echo "timeout 3" >> /boot/loader/loader.conf
-echo "editor 0" >> /boot/loader/loader.conf
+# Bootloader installation
+bootctl install
+cat <<EOT > /boot/loader/loader.conf
+default arch
+timeout 3
+editor 0
+EOT
 
-# Create boot entry
-PARTUUID=$(blkid -s PARTUUID -o value $ROOT_PART)
-echo "title   Arch Linux" > /boot/loader/entries/arch.conf
-echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
-echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch.conf
-echo "options root=PARTUUID=$PARTUUID rw" >> /boot/loader/entries/arch.conf
+cat <<EOT > /boot/loader/entries/arch.conf
+title   Arch Linux LTS
+linux   /vmlinuz-linux-lts
+initrd  /initramfs-linux-lts.img
+options root=PARTUUID=$(blkid -s PARTUUID -o value $ROOT_PART) rw
+EOT
+
+# Create a new user
+useradd -m -G wheel $USERNAME
+echo "Set password for $USERNAME:"
+passwd $USERNAME
+
+# Configure sudo
+pacman -S --noconfirm sudo
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Enable necessary services
+systemctl enable systemd-networkd
+systemctl enable systemd-resolved
 
 EOF
 
-# 12. Unmount filesystems and reboot
-echo "Unmounting filesystems and rebooting..."
-umount -R $ROOT_MOUNT
+# Unmount and reboot
+umount -R /mnt
+echo "Installation complete. Rebooting in 5 seconds..."
+sleep 5
 reboot
-
-echo "Arch Linux installation with systemd-boot is complete!"
